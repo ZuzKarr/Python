e77c1fef0fb0c6af97411ea79aa7639827bb825e
ZuzKarr <115366701+ZuzKarr@users.noreply.github.com>
2022-11-28 19:31:40 +0100

    Add files via upload

--- person.py
+++ person.py
@@ -5,11 +5,47 @@ Created on Sat Nov 26 21:30:02 2022
 @author: Zu
 """
 
-person = {
-    'first_name': "beri", 'last_name': 'ronikier','age': 27, 'city': 'warsaw'                    }
+person_0 = {
+    'first_name': "beri",
+    'last_name': 'ronikier',
+    'age': 27,
+    'city': 'warsaw'                  
+    }
 
-print(person['first_name'])
-print(person['last_name'])
-print(person['age'])
-print(person['city'])
+person_1 = {
+    'first_name': "zuzia",
+    'last_name': 'plawska',
+    'age': 19,
+    'city': 'gdansk'                  
+    }
+
+person_2 = {
+    'first_name': "antek",
+    'last_name': 'plawski',
+    'age': 23,
+    'city': 'warsaw'                  
+    }
+  
+people = [person_0, person_1, person_2]
+
+for person in people:
+    print(person)
+
+for person in people:
+    name = person['first_name'].title() + '' + person['last_name'].title()
+    age = person['age']
+    city = person['city'].title()
+    
+    print(name +', of city ' +city +' is ' +str(age) +' years old.')
+
+
+
+
+
+
+
+
+
+
+
 
